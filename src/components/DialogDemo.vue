b9d5ab7445e7767ddc500d832ac1e1becd4efd1d
ak <123@qq.com>
2022-01-31 10:04:31 +0800

    Dialog 支持 visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,22 @@
-<template>
-
+ <template>
+  <div>Dialog示例</div>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="x"/>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
+
+export default {
+  components: {Dialog, Button},
+  setup() {
+    const x = ref(false);
+    const toggle = ()=>{
+      x.value = !x.value
+    }
+    return {x ,toggle};
+  }
+};
+</script>
