61051212a952d299f11fa6bde592ae94f9bb109c
ak <123@qq.com>
2022-01-31 09:25:43 +0800

    基本完成Button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,57 @@
 <template>
   <div>
     Button组件
+    <h1>示例一</h1>
     <Button >123</Button>
     <Button theme="button">123</Button>
     <Button theme="link" >123</Button>
     <Button theme="text" >123</Button>
+    <h1>示例二</h1>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>普普通</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">普普通</Button>
+      <Button size="small" theme="link">小小小</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大大大</Button>
+      <Button theme="text">普普通</Button>
+      <Button size="small" theme="text">小小小</Button>
+    </div>
+    <h1>示例3</h1>
+    <div>
+      <div><Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="link" level="main">主要链接按钮</Button>
+        <Button theme="link">普通链接按钮</Button>
+        <Button theme="link" level="danger">危险链接按钮</Button>
+      </div>
+      <div>
+        <Button theme="text" level="main">主要文字按钮</Button>
+        <Button theme="text">普通文字按钮</Button>
+        <Button theme="text" level="danger">危险文字按钮</Button>
+      </div>
+    </div>
+    <h1>示例4</h1>
+    <div>
+      <Button disabled>禁用按钮</Button>
+      <Button theme="link" disabled>禁用链接按钮</Button>
+      <Button theme="text" disabled>禁用文字</Button>
+    </div>
+    <h1>示例5</h1>
+    <div>
+      <Button loading>加载中</Button>
+    </div>
+    <div>
+      <Button >加载完毕</Button>
+    </div>
   </div>
 </template>
 <script lang="ts">
