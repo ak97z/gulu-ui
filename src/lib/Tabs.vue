3177ee9602c7bbf1ed4e7375ca36f1761f5f3d96
ak <123@qq.com>
2022-02-02 22:09:42 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" @click="select(t)" v-for="(t,index) in titles" :key="index"
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item"
+           :ref="el=>{if(el)navItems[index]=el}"
+           @click="select(t)"
+           v-for="(t,index) in titles"
+           :key="index"
            :class="{selected:t===selected}">
         {{ t }}
       </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component class="gulu-tabs-content-item"
@@ -15,7 +20,7 @@
 </template>
 <script lang="ts">
 import Tab from '../lib/Tab.vue';
-import {computed} from 'vue';
+import {computed, onMounted, onUpdated, ref} from 'vue';
 
 export default {
   props: {
@@ -24,8 +29,26 @@ export default {
     },
   },
   setup(props, context) {
-    console.log(context.slots.default());
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+
+
+
+    const x =() => {
+      const divs = navItems.value;
+      const result = divs.filter(div => div.classList.contains('selected'))[0];
+      const {width} = result.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {left: left1} = container.value.getBoundingClientRect();
+      const {left: left2} = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + 'px';
+    }
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
+
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs组件的子组件必须是Tab');
@@ -43,7 +66,7 @@ export default {
       context.emit('update:selected', title);
     };
     return {
-      defaults, titles, current, select
+      defaults, titles, current, select, navItems, indicator,container,
     };
   }
 };
@@ -59,6 +82,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -73,13 +97,25 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
   }
 
   &-content {
     padding: 8px 0;
-    &-item{
+
+    &-item {
       display: none;
-      &.selected{
+
+      &.selected {
         display: block;
       }
     }
