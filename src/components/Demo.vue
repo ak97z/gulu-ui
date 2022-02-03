c9db45b79baf009ef040d43fb04fd2007d3e2720
ak <123@qq.com>
2022-02-03 22:54:18 +0800

    显示demo标题

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,35 +1,34 @@
 <template>
-  <div>
-    <h1>Switch 组件示例 </h1>
+  <div class="demo">
+    <h2>{{component.__sourceCodeTitle}}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
+    </div>
 
-    <Demo :component="Switch1Demo" />
-    <Demo :component="Switch2Demo" />
   </div>
 </template>
+
 <script lang="ts">
-import Switch from '../lib/Switch.vue';
-import Button from '../lib/Button.vue';
-import {ref} from 'vue';
-import Switch1Demo from '../components/Switch1.demo.vue';
-import Switch2Demo from '../components/Switch2.demo.vue';
-
+import Button from '../lib/Button.vue'
 import 'prismjs';
-import 'prismjs/themes/prism.css';
-import Demo from './Demo.vue';
-const Prism=(window as any).Prism
-
+import 'prismjs/themes/prism.css'
+const Prism = (window as any).Prism
 export default {
-  components: {
-    Demo,
-    Switch, Button
+  props: {
+    component: Object
   },
   setup() {
-    const bool = ref(false);
     return {
-      bool, Switch1Demo, Switch2Demo,Prism
-    };
+      Prism
+    }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -37,27 +36,22 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-
-  > h2 {
+  >h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
   }
-
   &-component {
     padding: 16px;
   }
-
   &-actions {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
   }
-
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-
-    > pre {
+    >pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;
