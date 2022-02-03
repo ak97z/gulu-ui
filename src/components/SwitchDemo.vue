ca631ba6fbdf050bb06b7711fb0b72f29cee25a6
ak <123@qq.com>
2022-02-03 23:20:14 +0800

    优化 Demo,查看代码按钮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,32 +1,25 @@
 <template>
   <div>
     <h1>Switch 组件示例 </h1>
-
-    <Demo :component="Switch1Demo" />
-    <Demo :component="Switch2Demo" />
+    <Demo :component="Switch1Demo"/>
+    <Demo :component="Switch2Demo"/>
   </div>
 </template>
 <script lang="ts">
-import Switch from '../lib/Switch.vue';
-import Button from '../lib/Button.vue';
-import {ref} from 'vue';
+
 import Switch1Demo from '../components/Switch1.demo.vue';
 import Switch2Demo from '../components/Switch2.demo.vue';
-
-import 'prismjs';
-import 'prismjs/themes/prism.css';
 import Demo from './Demo.vue';
-const Prism=(window as any).Prism
+
 
 export default {
   components: {
     Demo,
-    Switch, Button
   },
   setup() {
-    const bool = ref(false);
+
     return {
-      bool, Switch1Demo, Switch2Demo,Prism
+      Switch1Demo, Switch2Demo
     };
   }
 };
