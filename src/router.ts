2f0780b240e94eb931b0700cac0ddfdcfa39387e
ak <123@qq.com>
2022-02-04 10:06:54 +0800

    解决上节课的遗留问题

--- src/router.ts
+++ src/router.ts
@@ -5,11 +5,14 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
+import intro from './markdown/intro.md';
+import getStarted from './markdown/get-started.md';
+import install from './markdown/install.md';
 import {h} from 'vue';
 import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory();
-const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
+const md = string => h(Markdown, {content: string, key: string});
 export const router = createRouter({
     history: history,
     routes: [
@@ -17,9 +20,9 @@ export const router = createRouter({
         {
             path: '/doc', component: Doc, children: [
                 {path: '', redirect: '/doc/intro'},
-                {path: 'intro', component: md('intro')},
-                {path: 'get-started', component: md('get-started')},
-                {path: 'install', component: md('install')},
+                {path: 'intro', component: md(intro)},
+                {path: 'get-started', component: md(getStarted)},
+                {path: 'install', component: md(install)},
                 {path: 'switch', component: SwitchDemo},
                 {path: 'button', component: ButtonDemo},
                 {path: 'dialog', component: DialogDemo},
