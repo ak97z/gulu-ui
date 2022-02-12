a6bfb27b13422d1c66ba60631407e9bebb6b764f
ak <123@qq.com>
2022-02-12 21:13:08 +0800

    做了Icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,15 @@
+<template>
+ <svg class="icon">
+  <use :xlink:href="`#icon-${icon}`"></use>
+</svg>
+</template>
+
+<script>
+export default {
+  props:{
+    icon:{
+      type:String,
+    }
+  }
+}
+</script>
