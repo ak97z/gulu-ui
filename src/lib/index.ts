2ecc03fd2a286d852a4d168d1c91dd2c30563b21
ak <123@qq.com>
2022-03-14 11:55:38 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+export {default as Switch} from './Switch.vue';
+export {default as Button} from './Button.vue';
+export {default as Dialog} from './Dialog.vue';
+export {default as Tabs} from './Tabs.vue';
+export {default as Input} from './Input.vue';
+export {default as Row} from './Row.vue';
+export {default as Col} from './Col.vue';
+export {default as Layout} from './Layout.vue';
+export {default as Popover} from './Popover.vue';
+export {default as Collapse} from './Collapse.vue';
